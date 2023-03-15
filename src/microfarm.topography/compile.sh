bff254e42110de2d78fa62e968d1293b5bae20e9
Souheil Chelfouh <trollfot@gmail.com>
2023-03-15 20:21:23 +0100

    fixed naming

--- src/microfarm.topography/compile.sh
+++ src/microfarm.topography/compile.sh
@@ -1,1 +1,1 @@
-protoc --experimental_allow_proto3_optional --python_out=src/microfarm/topography/messages definitions/notification.proto definitions/requests.proto definitions/persistence.proto
+protoc --experimental_allow_proto3_optional --python_out=src/microfarm/topography messages/notification.proto messages/requests.proto messages/persistence.proto
