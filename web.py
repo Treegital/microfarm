0dec3d09bd340ac092c8f9d8be2ab90e0aaf11ae
Souheil Chelfouh <trollfot@gmail.com>
2023-03-23 16:31:43 +0100

    Fixed webapp

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ from sanic import Sanic
 from sanic_ext import validate
 from dataclasses import dataclass, asdict
 from sanic.response import text, json
+from contextlib import asynccontextmanager
 
 
 @dataclass
@@ -31,7 +32,7 @@ class TokenRequest:
 def rpcservice(bind: str):
 
     @asynccontextmanager
-    def service(self):
+    async def service():
         client = await rpc.connect_rpc(connect=bind)
         try:
             yield client.call
@@ -42,17 +43,17 @@ def rpcservice(bind: str):
 
 
 app = Sanic("Microfarm")
-app.ctx.jwt = service('tcp://127.0.0.1:5556')
-app.ctx.pki = service('tcp://127.0.0.1:5556')
-app.ctx.courrier = service('tcp://127.0.0.1:5556')
-app.ctx.accounts = service('tcp://127.0.0.1:5556')
-app.ctx.websockets = service('tcp://127.0.0.1:5556')
+app.ctx.courrier = rpcservice('tcp://127.0.0.1:5100')
+app.ctx.jwt = rpcservice('tcp://127.0.0.1:5200')
+app.ctx.accounts = rpcservice('tcp://127.0.0.1:5300')
+app.ctx.pki = rpcservice('tcp://127.0.0.1:5400')
+app.ctx.websockets = rpcservice('tcp://127.0.0.1:5500')
 
 
 @app.post("/account/new")
 @validate(json=Registration)
 async def register(request, body: Registration):
-    with app.ctx.accounts.service() as service:
+    async with app.ctx.accounts() as service:
         response = await service.create_account(asdict(body))
     return json(response)
 
@@ -60,7 +61,7 @@ async def register(request, body: Registration):
 @app.post("/account/verify")
 @validate(json=AccountVerification)
 async def verify(request, body: AccountVerification):
-    with app.ctx.accounts.service() as service:
+    async with app.ctx.accounts() as service:
         response = await service.verify_account(
             body.email,
             body.token
@@ -71,17 +72,19 @@ async def verify(request, body: AccountVerification):
 @app.post("/account/verify/token")
 @validate(json=TokenRequest)
 async def request_verification_token(request, body: TokenRequest):
-    with app.ctx.accounts.service() as service:
-        response = await service.call.request_account_token(body.email)
+    async with app.ctx.accounts() as service:
+        response = await service.request_account_token(body.email)
     return json(response)
 
 
 @app.post("/login")
 @validate(json=Login)
 async def login(request, body: Login):
-    with app.ctx.accounts.service() as service:
+    async with app.ctx.accounts() as service:
         response = await service.verify_credentials(
             body.email,
             body.password
         )
+    async with app.ctx.jwt() as service:
+        response = await service.get_token(response)
     return json(response)
